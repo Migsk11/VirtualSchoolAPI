3990ca20d20503f72c7973e68bc10deb2ab9a024
Migsk11 <77deku@gmail.com>
2026-06-27 10:49:04 -0300

    - fast fix: Cors

--- src/main/java/com/api/TechLearnAPI/config/CorsConfig.java
+++ src/main/java/com/api/TechLearnAPI/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Libera todos os endpoints
-            .allowedOrigins("http://localhost:5173", "https://cuddly-bassoon-97w4596vqjpwhp9rg-5173.app.github.dev/") // O domínio do seu frontend
+            .allowedOrigins("http://localhost:5173", "https://cuddly-bassoon-97w4596vqjpwhp9rg-5173.app.github.dev/", "http://127.0.0.1:5175") // O domínio do seu frontend
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD") // Métodos permitidos
             .allowedHeaders("*") // Permite todos os headers
             .allowCredentials(true); // Permite envio de cookies/auth se necessário
